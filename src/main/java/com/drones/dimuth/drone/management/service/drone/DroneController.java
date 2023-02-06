8f14763e6cd63a9a7ab7e082a8ce228f83dda814
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-06 23:25:01 +0530

    Add endpoints to add and get drones

---
+++ src/main/java/com/drones/dimuth/drone/management/service/drone/DroneController.java
@@ -0,0 +1,34 @@
+package com.drones.dimuth.drone.management.service.drone;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/drones")
+public class DroneController {
+
+    private final DroneService droneService;
+
+    @Autowired
+    public DroneController(DroneService droneService) {
+        this.droneService = droneService;
+    }
+
+    @GetMapping
+    public List<Drone> geAllDrones() {
+        return droneService.getAllDrones();
+    }
+
+    @PostMapping
+    public void addDrone(@RequestBody Drone drone) {
+
+        droneService.addDrone(drone);
+    }
+
+
+}
