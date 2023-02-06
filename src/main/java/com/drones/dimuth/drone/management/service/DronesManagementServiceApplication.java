8a7d3aa29e5527ead5f2a0affce7d91679533d32
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-06 23:08:45 +0530

    add spring boot application structure

---
+++ src/main/java/com/drones/dimuth/drone/management/service/DronesManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.drones.dimuth.drone.management.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesManagementServiceApplication.class, args);
+	}
+
+}
