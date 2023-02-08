f5ac296b37e2fe5dcf38ba0c6a23d3610f9cab6d
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-08 18:08:14 +0530

    add sample test class

---
+++ src/test/java/com/drones/dimuth/drone/management/service/DronesManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drones.dimuth.drone.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
