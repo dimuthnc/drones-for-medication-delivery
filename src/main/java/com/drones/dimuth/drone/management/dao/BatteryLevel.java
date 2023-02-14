9b61778a5341d8bc3da37b2e7aa144452501e2cb
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-14 14:26:05 +0530

    introduce BatteryLevel DAO class to represent the battery level query response

---
+++ src/main/java/com/drones/dimuth/drone/management/dao/BatteryLevel.java
@@ -0,0 +1,18 @@
+package com.drones.dimuth.drone.management.dao;
+
+public class BatteryLevel {
+
+    double batteryLevel;
+
+    public BatteryLevel(double batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public double getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(double batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+}
