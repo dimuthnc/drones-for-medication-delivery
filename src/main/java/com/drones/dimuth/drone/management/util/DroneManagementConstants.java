ec12a0a5f228ec6d944887a824ca7156e6920d74
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-14 22:29:32 +0530

    introduce constant class for storing application related constants

--- src/main/java/com/drones/dimuth/drone/management/util/DroneManagementConstants.java
+++ src/main/java/com/drones/dimuth/drone/management/util/DroneManagementConstants.java
@@ -1,5 +1,7 @@
 package com.drones.dimuth.drone.management.util;
 
 public class DroneManagementConstants {
-    public static final double MIN_BATTERY_FOR_LOADING = 10.0;
+    public static final double MIN_BATTERY_FOR_LOADING = 25.0;
+    public static final int IMAGE_COLUMN_SIZE = 10000000;
+    public static final long AUDIT_FREQUENCY = 60000;
 }
