18afb39215f7a4333b68109fd8a7f09c78222ee3
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-14 14:23:42 +0530

    add weight and batteryLevel validation when adding a delivery

--- src/main/java/com/drones/dimuth/drone/management/service/DeliveryService.java
+++ src/main/java/com/drones/dimuth/drone/management/service/DeliveryService.java
@@ -4,6 +4,7 @@ import com.drones.dimuth.drone.management.dao.Delivery;
 import com.drones.dimuth.drone.management.dao.Drone;
 import com.drones.dimuth.drone.management.dao.Medication;
 import com.drones.dimuth.drone.management.dao.MedicationDelivery;
+import com.drones.dimuth.drone.management.exception.DroneManagementServiceException;
 import com.drones.dimuth.drone.management.model.DroneState;
 import com.drones.dimuth.drone.management.repository.DeliveryRepository;
 import java.util.List;
@@ -46,22 +47,43 @@ public class DeliveryService {
     }
 
     @Transactional
-    public void addDelivery(Delivery delivery) {
-        Delivery savedDelivery = deliveryRepository.save(delivery);
-        List<MedicationDelivery> medicationDeliveries = delivery.getMedicationDeliveries();
-        for (MedicationDelivery medicationDelivery : medicationDeliveries) {
-            medicationDelivery.setDelivery(savedDelivery);
-            Optional<Medication> medication =
-                    medicationService.getMedicationByCode(medicationDelivery.getMedication().getCode());
-            if (medication.isPresent()) {
-                medicationDelivery.setMedication(medication.get());
-                saveMedicationDelivery(medicationDelivery);
+    public void addDelivery(Delivery delivery) throws DroneManagementServiceException {
+        Optional<Drone> drone = droneService.findDroneBySerialNumber(delivery.getDrone().getSerialNumber());
+        if(drone.isPresent() && drone.get().getBatteryLevel() > 10) {
+            Delivery savedDelivery = deliveryRepository.save(delivery);
+            List<MedicationDelivery> medicationDeliveries = delivery.getMedicationDeliveries();
+            double droneWeightLimit = drone.get().getWeightLimit();
+            double weightSum = 0;
+            for (MedicationDelivery medicationDelivery : medicationDeliveries) {
+                medicationDelivery.setDelivery(savedDelivery);
+                Optional<Medication> medication =
+                        medicationService.getMedicationByCode(medicationDelivery.getMedication().getCode());
+                if (medication.isPresent()) {
+                    weightSum += medication.get().getWeight();
+                    if(weightSum < droneWeightLimit) {
+                        medicationDelivery.setMedication(medication.get());
+                        saveMedicationDelivery(medicationDelivery);
+                    }
+                    else {
+                        throw new IllegalStateException("Drone Weight Limit Exceeded");
+                    }
+                }
+                else {
+                    throw new IllegalStateException("Medication not found");
+                }
+            }
+            droneService.updateDroneStatus(delivery.getDrone(), DroneState.LOADED);
+        }
+        else {
+            if(!drone.isPresent()) {
+                throw new DroneManagementServiceException("Drone not found");
             }
             else {
-                throw new IllegalStateException("Medication not found");
+                throw new IllegalStateException("Need more than 10% battery to load medication");
             }
+
         }
-        droneService.updateDroneStatus(delivery.getDrone(), DroneState.LOADED);
+
     }
 
     private void saveMedicationDelivery(MedicationDelivery medicationDelivery) {
