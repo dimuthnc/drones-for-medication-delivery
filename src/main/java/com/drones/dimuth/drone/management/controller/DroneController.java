0e3306dd92b5cb4b5ef2ce14beac084ea0e70e51
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2023-02-14 14:25:20 +0530

    add endpoints to check available drones and battery level of a drone

--- src/main/java/com/drones/dimuth/drone/management/controller/DroneController.java
+++ src/main/java/com/drones/dimuth/drone/management/controller/DroneController.java
@@ -1,10 +1,13 @@
 package com.drones.dimuth.drone.management.controller;
 
+import com.drones.dimuth.drone.management.dao.BatteryLevel;
 import com.drones.dimuth.drone.management.dao.Drone;
+import com.drones.dimuth.drone.management.exception.DroneManagementServiceException;
 import com.drones.dimuth.drone.management.service.DroneService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +29,19 @@ public class DroneController {
         return droneService.getAllDrones();
     }
 
+    @GetMapping("available")
+    public List<Drone> getAvailableDrones() {
+        return droneService.getAllAvailableDrones();
+    }
+
     @PostMapping
     public void addDrone(@RequestBody Drone drone) {
         droneService.addDrone(drone);
     }
 
+    @GetMapping("{droneSerialNumber}/battery")
+    public BatteryLevel getDroneBatteryLevel(@PathVariable String droneSerialNumber) throws DroneManagementServiceException {
+        return droneService.getDroneBatteryLevel(droneSerialNumber);
+    }
 
 }
